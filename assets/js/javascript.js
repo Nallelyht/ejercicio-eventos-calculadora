5a10a7133077e62b56ae7778d48c2661f1c6e31f
Nallely House <nallelyht@gmail.com>
2017-03-23 17:05:51 -0600

    formato

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,12 +2,18 @@
 function boton(num) {
   var numero = document.calculadora.resultado.value;
   document.calculadora.resultado.value = numero + num;
+  var numero = document.calculadora.resultado2.value;
+  document.calculadora.resultado2.value = numero + num;
+  console.log(document.calculadora.resultado.value )
 };
 function borrar() {//funcion para limpar input
   document.calculadora.resultado.value ="";
+  document.calculadora.resultado2.value ="";
 };
-
-}
 function calcular(){//funcion para mostrar resultado
   document.calculadora.resultado.value = eval(document.calculadora.resultado.value);
 };
+function cuadrado() {
+  var alCuadrado=Math.pow(eval(document.calculadora.resultado.value),2);
+  console.log(alCuadrado);
+}
---
+++ assets/js/js2.js
@@ -0,0 +1,4 @@
+function boton(num) {
+  var numero = document.calculadora.resultado.value;
+  document.calculadora.resultado.value = numero + num;
+};
